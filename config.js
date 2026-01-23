2ffdfa5569e1bcff57430e3b0d016e7bf168e9f0
dhbfdffgb <saniyawalaha@gmail.com>
2026-01-23 20:14:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?WUdCck'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?fMgfB7'
 };
