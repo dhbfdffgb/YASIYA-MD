2421d96d4d248a0e90d808993d6dfe7120482cae
dhbfdffgb <saniyawalaha@gmail.com>
2026-05-02 09:28:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?TfClCP',
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?DpOcrI',
     PORT: process.env.PORT || 8000,
     SESSION_NAME: process.env.SESSION_NAME || "auth_info_baileys"
 };
